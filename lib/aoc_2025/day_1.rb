7a39e9e385959d8d6ebd521c3d2dc72ad622f5e5
Chris Severns <severnsc@gmail.com>
2025-12-02 09:31:31 -0600

    solve day 1 part 1

--- lib/aoc_2025/day_1.rb
+++ lib/aoc_2025/day_1.rb
@@ -1,3 +1,7 @@
 module DayOne
   require_relative "day_1/part_1"
+
+  def solve_part_one
+    PartOne.solve
+  end
 end
--- lib/aoc_2025/day_1/part_1.rb
+++ lib/aoc_2025/day_1/part_1.rb
@@ -1,4 +1,15 @@
-module PartOne
-  require_relative "part_1/safe"
-  require_relative "part_1/secret_code"
+module DayOne
+  module PartOne
+    require_relative "part_1/safe"
+    require_relative "part_1/secret_code"
+
+    def self.solve
+      file = File.read "part_1/input.csv"
+      instructions = file.each_line.map(&:chomp)
+      safe = Safe.new dial_range: 0..99, dial_pointing_at: 50
+      secret_code = SecretCode.new safe:, secret_number: 0
+      secret_code.process_instructions instructions
+      p secret_code.value
+    end
+  end
 end
